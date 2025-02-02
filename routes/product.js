850bc43ca8e70e57fec2d8afab872e4d2f15b4aa
redaelemawy <redaelemawy98@gmail.com>
2025-02-03 01:51:12 +0200

    return prodcut description with products

--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const products = require('../data/products');
+const products = require("../data/products");
 
-router.get('/', (req, res) => {
-  const simplifiedProducts = products.map(product => ({
+router.get("/", (req, res) => {
+  const simplifiedProducts = products.map((product) => ({
     id: product.id,
     title: product.title, // Replace name with title
     price: product.price,
@@ -13,20 +13,21 @@ router.get('/', (req, res) => {
     category: product.category,
     stock: product.stock,
     isNew: product.isNew, // Include isNew property
-    discount: product.discount // Include discount property
+    discount: product.discount, // Include discount property
+    description: product.description,
   }));
-  
+
   res.json(simplifiedProducts);
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const productId = parseInt(req.params.id);
-  const product = products.find(p => p.id === productId);
-  
+  const product = products.find((p) => p.id === productId);
+
   if (!product) {
-    return res.status(404).json({ message: 'Product not found' });
+    return res.status(404).json({ message: "Product not found" });
   }
-  
+
   res.json(product);
 });
 
